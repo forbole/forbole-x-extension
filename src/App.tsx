a1e6a2612d48b204f62df4d4a46f061b032480f5
Calvin Kei <calvin.conceptz@gmail.com>
2021-05-12 20:17:55 +0800

    adjusted styles

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ const App: React.FC = () => {
     <iframe
       title="Forbole X"
       height={600}
-      width={480}
+      width={520}
       src={`${process.env.REACT_APP_WEB_APP_BASE_URL}/wallets?hide-menu=true`}
     />
   )
