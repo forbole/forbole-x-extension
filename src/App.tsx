b5db8331c8028661ffa11f17a51add202b23f0e5
Calvin Kei <calvin.conceptz@gmail.com>
2021-11-10 13:15:32 +0800

    update query params

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import qs from 'query-string'
 
 const App: React.FC = () => {
   const query = {
-    hideMenu: true,
+    isChromeExt: true,
     ...qs.parse(window.location.search),
   }
   const url = qs.stringifyUrl({ url: `${process.env.REACT_APP_WEB_APP_BASE_URL}/wallets`, query })
