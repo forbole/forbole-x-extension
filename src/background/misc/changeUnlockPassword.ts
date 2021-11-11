217aa002c38a06d2c6435c651ab48072d9551fe2
Calvin Kei <calvin.conceptz@gmail.com>
2021-11-12 00:57:33 +0800

    fix change unlock password issue

--- src/background/misc/changeUnlockPassword.ts
+++ src/background/misc/changeUnlockPassword.ts
@@ -3,7 +3,7 @@ import { getAccounts } from '../models/accounts'
 import { getWallets } from '../models/wallets'
 
 const changeUnlockPassword = async (oldPassword: string, password: string): Promise<void> => {
-  const wallets = await getWallets(oldPassword)
+  const wallets = await getWallets(oldPassword, true)
   const accounts = await getAccounts(oldPassword)
   const encryptedWalletsString = CryptoJS.AES.encrypt(JSON.stringify(wallets), password).toString()
   const encryptedAccountsString = CryptoJS.AES.encrypt(
